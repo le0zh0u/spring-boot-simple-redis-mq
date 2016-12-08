2c5a2b0fd7709671c2abfbea8e9f06f05d40a678
leozhou <492753662@qq.com>
2016-12-08 11:48:59 +0800

    add receiver

---
+++ src/main/java/com/leozhou/redis/Receiver.java
@@ -0,0 +1,27 @@
+package com.leozhou.redis;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by zhouchunjie on 2016/12/8.
+ */
+public class Receiver {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
+
+    private CountDownLatch latch;
+
+    @Autowired
+    public Receiver(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    public void receiveMessage(String message) {
+        LOGGER.info("Received <" + message + ">");
+        latch.countDown();
+    }
+}

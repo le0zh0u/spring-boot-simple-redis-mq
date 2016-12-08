8d905df143c6559ee107d76016742944799811c5
leozhou <492753662@qq.com>
2016-12-08 11:11:09 +0800

    init spring boot

---
+++ src/main/java/com/leozhou/SpringBootSimpleRedisMqApplication.java
@@ -0,0 +1,12 @@
+package com.leozhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimpleRedisMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSimpleRedisMqApplication.class, args);
+	}
+}
